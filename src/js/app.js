99870a1fe730bc8938199f61e09ebc6dbaca578a
Dmitry Shamrilo <shamrilods@gmail.com>
2021-11-17 23:32:20 +0300

    fixed main

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,6 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import * as dat from "dat.gui";
-import { FontLoader } from "three/examples/jsm/loaders/FontLoader";
-import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry";
 import Stats from "stats.js";
 
 const FOV = 75;
