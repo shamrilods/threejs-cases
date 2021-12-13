d56587885e1b0dc29bca1e88300c2fd48678a32f
Dmitry Shamrilo <shamrilods@gmail.com>
2021-12-13 22:02:11 +0300

    add particles

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import * as dat from "dat.gui";
 import Stats from "stats.js";
+import { Clock } from "three";
 
 const FOV = 75;
 const NEAR = 0.1;
@@ -23,39 +23,71 @@ class App {
     this.renderer = new THREE.WebGLRenderer({
       canvas: document.querySelector("canvas.webgl"),
     });
-    this.gui = new dat.GUI();
+    // this.gui = new dat.GUI();
     this.stats = new Stats();
+    this.textureLoader = new THREE.TextureLoader();
+    this.clock = new Clock();
   }
 
   init() {
     this.renderer.setSize(this.sizes.width, this.sizes.height);
     this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-    this.camera.position.z = 3;
+    this.camera.position.z = 1;
     this.scene.add(this.camera);
-
-    this.axesHelper = new THREE.AxesHelper(2);
-    this.scene.add(this.axesHelper);
 
     this.controls = new OrbitControls(this.camera, this.renderer.domElement);
     // Более плавное перемещение
     this.controls.enableDamping = true;
 
-    this.addMesh();
-    this.addGui();
+    this.addParticles();
     this.render();
     document.body.appendChild(this.stats.dom);
     window.addEventListener("resize", this.windowResizeHandler.bind(this));
   }
 
-  addMesh() {
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: "red" });
-    this.scene.add(new THREE.Mesh(geometry, material));
-  }
+  addParticles() {
+    this.particlesGeometry = new THREE.BufferGeometry();
+    this.particlesCount = 5000;
+    const positions = new Float32Array(this.particlesCount * 3);
+    const colors = new Float32Array(this.particlesCount * 3);
 
-  addGui() {
-    this.gui.add(this.axesHelper, "visible").name("axesHelper");
+    for (let i = 0; i < positions.length; i++) {
+      positions[i] = (Math.random() - 0.5) * 10;
+      colors[i] = Math.random();
+    }
+
+    this.particlesGeometry.setAttribute(
+      "position",
+      new THREE.BufferAttribute(positions, 3)
+    );
+
+    this.particlesGeometry.setAttribute(
+      "color",
+      new THREE.BufferAttribute(colors, 3)
+    );
+
+    const particleTexture = this.textureLoader.load(
+      "/3d/textures/particles.png"
+    );
+
+    const particlesMaterial = new THREE.PointsMaterial({
+      size: 0.15,
+      sizeAttenuation: true,
+      alphaMap: particleTexture,
+      transparent: true,
+      alphaTest: 0.01,
+      depthWrite: false,
+      blending: THREE.AdditiveBlending,
+    });
+
+    particlesMaterial.vertexColors = true;
+
+    this.particles = new THREE.Points(
+      this.particlesGeometry,
+      particlesMaterial
+    );
+    this.scene.add(this.particles);
   }
 
   windowResizeHandler() {
@@ -77,6 +109,7 @@ class App {
   render() {
     this.controls.update();
     this.stats.update();
+
     this.renderer.render(this.scene, this.camera);
     requestAnimationFrame(this.render.bind(this));
   }
